6111b52f746290732c9874dbb664f00fd5e3261c
Shahidul Islam <shahid.okay@gmail.com>
2022-04-09 15:06:25 +0000

    display addition question the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,29 @@
                 alert("You Clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 /**
  * The main game "loop", called when the script is first loaded.
  * and after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
 
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 ) + 1;
     let num2 = Math.floor(Math.random() * 25 ) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -42,7 +51,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
